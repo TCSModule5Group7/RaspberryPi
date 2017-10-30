33c29e19b03d8c44510cd9b4fd0a4263720dba1a
killera40 <arwin.sleutjes@gmail.com>
2017-10-30 11:58:04 +0100

    add some comments

--- Pong.py
+++ Pong.py
@@ -14,10 +14,10 @@ if __name__ == "__main__":
             host = sys.argv[1]
             port = int(sys.argv[2])
             Logger.log("Initializing queues")
-            q_spi_read = Queue()
-            q_spi_write = Queue()
-            q_tcp_read = Queue()
-            q_tcp_write = Queue()
+            q_spi_read = Queue()  # Stores data read from the spi interface
+            q_spi_write = Queue()  # Stores data to be written to the spi interface
+            q_tcp_read = Queue()  # Stores data read from the tcp interface
+            q_tcp_write = Queue()  # Stores data to be written to the tcp interface
             Logger.logspi("Initializing spi-server")
             spi_server = SPIServer(q_spi_read, q_spi_write, 0b00, 0, 0)
             Logger.logtcp("Initializing tcp-server")
