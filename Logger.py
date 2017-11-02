c8e91d9193362bbc74519294b3959a922d112a93
killera40 <arwin.sleutjes@gmail.com>
2017-11-02 11:46:09 +0100

    add intermediate standard in flushing and game logging

--- Logger.py
+++ Logger.py
@@ -1,21 +1,34 @@
+from sys import stdin
+from termios import tcflush, TCIFLUSH
+
 ansiRed = "\033[31m"
 ansiGreen = "\033[32m"
 ansiYellow = "\033[33m"
 ansiBlue = "\033[34m"
+ansiMagenta = "\033[35m"
 ansiEnd = "\033[0m"
 
 
 def log(message):
+    tcflush(stdin, TCIFLUSH)
     print(ansiBlue + "[log]" + message + ansiEnd)
 
 
-def logspi(message):
+def log_spi(message):
+    tcflush(stdin, TCIFLUSH)
     print(ansiYellow + "[spi]" + message + ansiEnd)
 
 
-def logtcp(message):
+def log_tcp(message):
+    tcflush(stdin, TCIFLUSH)
     print(ansiGreen + "[tcp]" + message + ansiEnd)
 
 
-def logerror(message):
+def log_game(message):
+    tcflush(stdin, TCIFLUSH)
+    print(ansiMagenta + "[game]" + message + ansiEnd)
+
+
+def log_error(message):
+    tcflush(stdin, TCIFLUSH)
     print(ansiRed + "[error]" + message + ansiEnd)
